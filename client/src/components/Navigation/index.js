73bc88cdb15bd71f206a4be31aab357a0235bc1b
User 7 <User 7@DESKTOP-7722ELG>
2022-03-26 17:12:56 -0500

    banner logo and color

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './styles.css';
 
+import logo from './retrotubing_logo.png';
+
 function Navigation() {
   return (
     <div className="navigation">
-      <h1 id="title">RetroTubing</h1>
+     <img 
+      src={logo} 
+      alt="retrotubing logo"
+      style={{ height: 50, width: 200, 
+        margin: 30  }}
+      
+    />
     </div>
   )
 }
