b3db564348040bab22fa7e32028bfd80d886357f
Nathaniel Brown <nateabrown@icloud.com>
2022-03-26 17:11:34 -0500

    add pixelation

--- client/src/components/Screen/index.js
+++ client/src/components/Screen/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 import constants from '../../constants';
+import Camera from '../Camera';
 
 const Container = styled.div`
   width: 500px;
   height: 500px;
 
   border: 5px dashed ${constants.darkbackground};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 ` 
 
 function Screen() {
   return (
     <Container>
-
+      <Camera />
     </Container>
   )
 }
