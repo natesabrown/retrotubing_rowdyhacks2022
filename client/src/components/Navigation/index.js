f00d11c3394a5b71c8b9e313c193c76d641147e9
Nathaniel Brown <nateabrown@icloud.com>
2022-03-26 15:00:58 -0500

    add example nav for allison

---
+++ client/src/components/Navigation/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <h1 id="title">RetroTubing</h1>
+    </div>
+  )
+}
+
+export default Navigation;
